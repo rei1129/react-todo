a5d32ac67b270a3929538a751c37dd2cd0e98def
rei1129 <freead9211@yahoo.co.jp>
2021-08-25 17:41:28 +0900

    todoリスト追加機能

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+  const App =() =>{
+    const Todolist =[
+      {
+        task: '',
+    },
+    ]
+    const [todos, setTodo] = useState(Todolist);
+    const [task,setTask] = useState('')
+
+    const addNewTask = (event) => {
+      setTask( event.target.value)
+  }
+
+  const Submit = (event) => {
+      event.preventDefault()
+      if(task === '') return
+      setTodo(todos => [...todos,{task}])
+      setTask('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>ToDoリスト</h1>
+        <form onSubmit={ Submit }>
+        こちらに入力し、エンターを押してください : <input value={ task }  onChange={addNewTask}/>
+        </form>
+        <ul>
+            { todos.map((todo, index) => (
+            <li key={ index }>{ todo.task } </li>
+            ))}
+        </ul>
     </div>
   );
-}
+ }
 
-export default App;
+ export default App;
